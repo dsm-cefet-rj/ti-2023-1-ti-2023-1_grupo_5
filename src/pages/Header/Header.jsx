9f3ae9a0370b4203174f33b62ec7b73181acb7f3
Danmok01 <rodrigomarquespf@gmail.com>
2023-04-04 03:01:26 -0300

    Header CSS

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,44 +1,70 @@
 import React from "react";
 import styles from "./Header.module.css";
-
+import { useState } from "react"
 import {  useCarrinho } from "../../context/CarrinhoContext";
+import { faCartShopping} from "@fortawesome/free-solid-svg-icons";
+import { faOutdent} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 import { Link } from "react-router-dom"
 
 
 export default function Header() {
+   
+    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    function handleMenuClick() {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+      function handleLinkClick() {
+        setIsMenuOpen(false);
+    }
     const carrinho = useCarrinho()
-  //  const itemsCount = Object.keys(carrinho.carrinho).length
-  const itemsCount = Object.keys(carrinho.carrinho).reduce((prev,curr) =>{
+    //  const itemsCount = Object.keys(carrinho.carrinho).length
+    const itemsCount = Object.keys(carrinho.carrinho).reduce((prev,curr) =>{
     return prev + carrinho.carrinho[curr].quantidade
-  },0)
-
-
+    },0)
+ 
+ 
     return (
+        
         <section className={styles.cabecalho}>
-            
-            <Link to=""><img src={"../public/images/logo.png"} alt="logo" className={styles.logo}/></Link>
+           
+            <Link to="" onClick={handleLinkClick}><img src={"../public/images/logo.png"} alt="logo" className={styles.logo}/></Link>
             <div>
+                <button className={styles.hamburger} onClick={handleMenuClick}><FontAwesomeIcon  icon={faOutdent} size="lg" style={{color: "#000000",}}/></button>
+                <div  className={styles.navigation_menu} >
                 <ul className={styles.navbar}>
-                    <form action="">
-                        <input type="search" placeholder="Digite sua pesquisa aqui" className={styles.pesquisar}/>
-                    </form>
+                  
                     <nav className={styles.header_li}>
-                        <li>
-                           <Link to="/login">Login</Link>
+                    <div  className={styles.navigation_menu}>
+                        <ul className={`${styles.navigation_menu_ul} ${isMenuOpen ? styles.open : ""}`}>
+                        <form action="">
+                        <input type="search" placeholder="Digite sua pesquisa aqui" className={styles.pesquisar}/>
+                        </form>
+                        <li className={styles.navigation_menu_li}>
+                           <Link to="/login" onClick={handleLinkClick}>Login</Link>
                         </li>
-                        <li>
-                           <Link to="/carrinho">
-                                Carrinho {' '}{itemsCount > 0 && <span>({itemsCount})</span>}
+                        <li className={styles.navigation_menu_li}>
+                           <Link to="/carrinho" onClick={handleLinkClick}><FontAwesomeIcon  icon={faCartShopping} size="lg" style={{color: "#000000",}}/>
+                                 {' '}{itemsCount > 0 && <span>({itemsCount})</span>}
                            </Link>
-                        </li>
-                        <li><Link to="usuario">dskdksikdi</Link></li>
+                        </li >
+                        <li className={styles.navigation_menu_li}>
+                            <Link to="usuario" onClick={handleLinkClick}></Link></li>
+                        </ul>
+                       
+                       </div> 
        
                     </nav>
-
+                    
                 </ul>
+                </div>
             </div>
+            
         </section>
     )
 }
