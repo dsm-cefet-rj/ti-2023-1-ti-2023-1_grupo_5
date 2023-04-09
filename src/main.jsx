4eba7228021089e76363bb9c21f876fc5c151bf4
Carlos <103267914+Carlos-H1307@users.noreply.github.com>
2023-04-09 03:22:46 -0300

    comit branch 2

--- src/main.jsx
+++ src/main.jsx
@@ -98,6 +98,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </React.StrictMode>,
 )
