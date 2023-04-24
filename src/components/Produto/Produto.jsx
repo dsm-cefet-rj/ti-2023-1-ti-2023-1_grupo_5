2e9a4775c0c747dc316ace688c7f37ab42059125
Carlos <103267914+Carlos-H1307@users.noreply.github.com>
2023-04-24 08:30:07 -0300

    carrinho mock db 2

--- src/components/Produto/Produto.jsx
+++ src/components/Produto/Produto.jsx
@@ -4,17 +4,22 @@ import adicionarAoCarrinho from "./adicionarAoCarrinho"
 
 //import p from "../components/Dados"
 import { useCarrinho } from "../../context/CarrinhoContext"
+import { useSelector, connect } from "react-redux"
 
 const Produto = ({produto}) => {
     const carrinho = useCarrinho()
+    const conta = useSelector((state) => state.conta)
+    const tipoLogin = useSelector((state) => state.tipoLogin)
+    
     const prod = produto
     function add(){
-        carrinho.addCarrinho(prod)
-        adicionarAoCarrinho(1, prod)
+        if(conta != null && tipoLogin === "cliente"){
+            carrinho.addCarrinho(prod)
+            adicionarAoCarrinho(conta.id, prod)
+        }
     }
     return(
         <div className={styles.novidades_produto}>
-            
             <Link to={`/produtos/${produto.id}`}>
                 <img src={produto.img} alt='' className={styles.novidade_produto_imagem} />
             </Link>
@@ -31,29 +36,4 @@ const Produto = ({produto}) => {
     )
 }
 
-export default Produto
-
-{/*
-export default function Produto(params) {
-    const carrinho = useContext(CarrinhoContext)
-    const add = Produto => () => {
-        carrinho.addCarrinho(Produto)
-    }
-    return (
-        <div className={styles.novidades_produto}>
-            <pre>{JSON.stringify(carrinho, null,2)}</pre>
-            <a> <Link to="/ProdutoSimples" >
-            <img src={params.produto.img} alt={params.produto.tipo} className={styles.novidade_produto_imagem} />
-            <div className={styles.novidade_produto_descricao}>{params.produto.descricao}</div>
-            <div className={styles.preco}>R$ {params.produto.preco}</div>
-            <div className={styles.metodo_pagamento}>À vista no pix</div>
-            
-            </Link></a>
-            <a>
-                <button className={styles.novidade_produto_botao} onClick={add(Produto)}>Comprar</button>
-            </a>
-        </div>
-    )
-}
-
-*/}
+export default connect()(Produto)
