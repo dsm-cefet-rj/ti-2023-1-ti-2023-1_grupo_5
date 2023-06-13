7847ce65188154d9a714836c190d94708afe7034
Marcos <marcs262002@gmail.com>
2023-06-13 05:15:13 -0300

    get e patch carrinhos

--- Backend/infoNeo/routes/carrinhos.js
+++ Backend/infoNeo/routes/carrinhos.js
@@ -6,26 +6,38 @@ let produtos = require('../models/produtos');
 /* GET users listing. */
 router.get('/:id', async (req, res, next) => {
   try {
-    let c = await carrinhos.findById(req.params.id)
+    let c = await carrinhos.findById(req.params.id).populate({path: 'produtos.produto', model: produtos});
 
-    console.log(c);
+    let filtrado = c.produtos.filter( (prod) => {
+      if(prod.produto != null){ return true }
+    });
+    
+    if(c.produtos.length != filtrado.length){
+      await carrinhos.findByIdAndUpdate(req.params.id, {produtos: filtrado});
+    }
 
     if(c != null){
       res.statusCode = 200;
-      res.json(c);
+      res.json(filtrado);
     }else{
        res.statusCode = 404;
        res.json({});
     }
   } catch (error) {
       res.statusCode = 500;
-      res.json({});
       console.log(error);
   }
 });
 
 router.patch('/:id', (req, res, next) => {
-
+  carrinhos.findByIdAndUpdate(req.params.id, {produtos: filtrado})
+  .then(() => {
+    res.statusCode = 200;
+    res.json({});
+  }).catch( (err) => {
+    res.statusCode = 500;
+    console.log(err);
+  })
 });
 
 module.exports = router;
